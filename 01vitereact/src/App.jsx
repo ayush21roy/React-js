f55f1165b2682ad426a05caaf1daefacf1c93fba
ayush21roy <4nn20cs012@nieit.ac.in>
2023-11-02 03:03:48 +0530

    basic-react-components-hooks-props

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <p>chai para</p>
+    </>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // hook - useState makes changes in UI
+  let [counter, setCounter] = useState(0);   //returns array of these 2 ->  // counter is value, setCounter is function and 15 is initial value
+  // let counter = 5;
+
+  const addValue = () => {
+    console.log(`clicked ${counter}`);
+    if (counter !== 20)
+      setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    if (counter !== 0)
+      setCounter(counter - 1);
+  }
+  return (
+    <>
+      <h1>02 react counter app</h1>
+      <h2>Counter : {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+
+      <p>COUNTER VALUE is : {counter}</p>
+    </>
+  )
+}
+
+export default App
